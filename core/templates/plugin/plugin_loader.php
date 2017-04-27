ceac9b8259b08124c75824468ef93f7b09b99722
Łukasz Wincencik <lukasz.wincencik@polcode.net>
2017-04-27 14:04:35 +0200

    fix activation errors, activate_app after db is created not before

--- core/templates/plugin/plugin_loader.php
+++ core/templates/plugin/plugin_loader.php
@@ -6,11 +6,7 @@ class <?php echo $name_camelized; ?>Loader extends MvcPluginLoader {
     var $tables = array();
 
     function activate() {
-    
-        // This call needs to be made to activate this app within WP MVC
-        
-        $this->activate_app(__FILE__);
-        
+            
         // Perform any databases modifications related to plugin activation here, if necessary
 
         require_once ABSPATH.'wp-admin/includes/upgrade.php';
@@ -20,6 +16,10 @@ class <?php echo $name_camelized; ?>Loader extends MvcPluginLoader {
         // Use dbDelta() to create the tables for the app here
         // $sql = '';
         // dbDelta($sql);
+        
+        // This call needs to be made to activate this app within WP MVC
+        
+        $this->activate_app(__FILE__);
         
     }
 
